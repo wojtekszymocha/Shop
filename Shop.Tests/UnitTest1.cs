d7feb408da845df9ca65707c3352a59aa53135fe
wojtek120071 <szymocha.wojtek@gmail.com>
2019-04-27 18:12:04 +0200

    Create project

---
+++ Shop.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shop.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
