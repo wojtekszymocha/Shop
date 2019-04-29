acdc6c789beb92f452e554c4435dd2803977426d
wojtek120071 <szymocha.wojtek@gmail.com>
2019-04-29 14:59:41 +0200

    Added Data Access Layer and changed Table Name Convention

---
+++ Shop/DataAccessLayer/CoursesContext.cs
@@ -0,0 +1,31 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Shop.DataAccessLayer
+{
+    public class CoursesContext: DbContext
+    {
+        public CoursesContext() : base("CoursesContext")
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ItemOrder> ItemsOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // this function turn off convention which automatically create plural for name of table in our DB
+            // does not add "ES" in the end name of table
+            // I created plular properties, I dont want something like Courses -> Courseses
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
